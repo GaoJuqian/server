f5ef001ba1dbc197df149d87a5bab2c197c238c1
cyRenShui <jqiown@163.com>
2023-02-27 21:43:13 +0800

    init

---
+++ src/main/java/com/gaojuqian/server/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.gaojuqian.server.config;
+
+import com.gaojuqian.server.interceptor.GlobalInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 添加拦截器
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GlobalInterceptor()).addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/gaojuqian/server/interceptor/GlobalInterceptor.java
@@ -0,0 +1,17 @@
+package com.gaojuqian.server.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 全局拦截器
+ */
+public class GlobalInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("拦截器");
+        return true;
+    }
+
+}
